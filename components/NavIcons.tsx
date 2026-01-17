92de24f0acef1c93aeb1d6f8065210f740f64d1e
nazim <nazimdev10022001@gmail.com>
2026-01-17 23:41:49 +0600

    refactor: extract wishlist icon into a dedicated `WhishListIcon` component.

--- components/NavIcons.tsx
+++ components/NavIcons.tsx
@@ -4,6 +4,7 @@ import CartIcon from "./CartIcon";
 import DashboardIcon from "./DashboardIcon";
 import NotificationIcon from "./NotificationIcon";
 import ProfileIcon from "./ProfileIcon";
+import WhishListIcon from "./WhishListIcon";
 
 const NavIcons = async () => {
   const user = await auth();
@@ -13,20 +14,7 @@ const NavIcons = async () => {
       {user?.user ? <ProfileIcon /> : <Link href={"/signin"}>Login</Link>}
       {user?.user && <NotificationIcon />}
       <Link href={"/wishlist"}>
-        <div className="relative">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="cursor-pointer w-5 h-5 md:w-6 md:h-6"
-          >
-            <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
-          </svg>
-        </div>
+        {user?.user && <WhishListIcon />}
       </Link>
       <Link href={cartHref}>
         {user?.user && <CartIcon isLoggedIn={!!user?.user} />}
